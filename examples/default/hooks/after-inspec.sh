4f4f259f76e92b35db98b662dad71611df1f584e
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-09-21 23:30:39 +0200

    testing

---
+++ examples/default/hooks/after-inspec.sh
@@ -0,0 +1,14 @@
+echo "REPO CLEANUP"
+echo "------------"
+
+echo "Delete all Gitlab repos"
+repos_ids=$(curl -fsSL -H "Private-Token: $GITLAB_TOKEN" 'https://gitlab.com/api/v4/projects?min_access_level=30'|jq '.[].id')
+for id in $repo_ids; do
+  curl -fsSL -H "Private-Token: $GITLAB_TOKEN" -X DELETE "https://gitlab.com/api/v4/projects/$id"
+done
+
+echo "Delete all Github repos"
+repos=$(curl -fsSL -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/user/repos"|jq -r '.[] | .owner.login + "/" + .name')
+for repo in $repos; do
+  curl -fsSL -H "Authorization: token $GITHUB_TOKEN" -X DELETE "https://api.github.com/repos/$repo"
+done
